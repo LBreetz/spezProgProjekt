437b13b23143f40cc7002f870a21be16c97eae33
L.Breetz <Luca.Breetz@Student.HTW-Berlin.de>
2021-04-29 19:08:11 +0200

    login site

--- app/controllers/TodoList.scala
+++ app/controllers/TodoList.scala
@@ -8,6 +8,10 @@ import javax.inject.{Inject, Singleton}
 @Singleton
 class TodoList @Inject()(val controllerComponents: ControllerComponents) extends BaseController{
 
+  def validateLoginGet(username: String, password: String) = Action {
+    Ok(s"$username logged in!")
+  }
+
   def todoList = Action {
     val todo = List("ToDo1", "ToDo2", "ToDo3", "ToDo4")
     Ok(views.html.TodoListIndex(todo))
